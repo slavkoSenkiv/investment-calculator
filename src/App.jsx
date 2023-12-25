8942d2667d1798dd3f693570b711c9264c04c967
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-12-25 08:50:21 +0100

    make forecasting arr update  on every input change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import InputZone from "./components/InputZone";
 import ForecTable from "./components/ForecTable";
 import { useState } from "react";
 
+import { calculateInvestmentResults } from "./util/investment";
+
 const defObjData = {
   initialInvestment: 11111,
   annualInvestment: 222,
@@ -9,18 +11,41 @@ const defObjData = {
   duration: 4,
 };
 
+const defForec = calculateInvestmentResults(
+  defObjData["initialInvestment"],
+  defObjData["annualInvestment"],
+  defObjData["expectedReturn"],
+  defObjData["duration"]
+);
+
 function App() {
   const [objectData, setObjectData] = useState(defObjData);
+  const [annualData, setAnnualData] = useState(defForec);
+
   function handlePropertyUpdate(propertyName, value) {
     setObjectData((prevObject) => ({
       ...prevObject,
       [propertyName]: value,
     }));
+
+    setAnnualData(() => {
+      return calculateInvestmentResults({
+        initialInvestment: objectData["initialInvestment"],
+        annualInvestment: objectData["annualInvestment"],
+        expectedReturn: objectData["expectedReturn"],
+        duration: objectData["duration"],
+      });
+    });
+
+    console.log(annualData);
   }
 
   return (
     <>
-      <InputZone handlePropertyUpdate={handlePropertyUpdate} objectData={objectData}/>
+      <InputZone
+        handlePropertyUpdate={handlePropertyUpdate}
+        objectData={objectData}
+      />
 
       <ForecTable />
     </>
